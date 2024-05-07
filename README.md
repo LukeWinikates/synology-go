81d54b290fc48516ed98dcb5b47d6b0dd38a6f9c
Luke Winikates <luke.winikates@gmail.com>
2024-05-07 16:04:30 -0700

    move from environment variables to a config file

--- README.md
+++ README.md
@@ -5,10 +5,8 @@ Examples:
 
 `dsmctl docker containers list`
 
-`dsmctl docker container restart $FOO`
+`dsmctl docker container restart --name $FOO`
 
-`dsmctl docker container stop $FOO`
+`dsmctl docker container start --name $FOO`
 
-`dsmctl docker container start $FOO`
-
-`dsmctl docker container logs $FOO`
+`dsmctl docker container logs --name $FOO`
--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/adrg/xdg"
+	"gopkg.in/yaml.v3"
+	"log"
 	"os"
 
 	"github.com/LukeWinikates/synology-go/cmd/docker"
@@ -9,33 +12,51 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(docker.Cmd(func() api.Client {
-		c, err := api.NewClient(
-			os.Getenv("DSM_HOST"),
-			os.Getenv("DSM_ACCOUNT"),
-			os.Getenv("DSM_PWD"))
-		if err != nil {
-			panic(err)
-		}
-		_, err = c.Login()
-		if err != nil {
-			panic(err)
-		}
-		return c
-	}))
-}
-
-var rootCmd = &cobra.Command{
-	Use: "dsmctl",
-	Long: `
+func rootCmd(c *config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "dsmctl",
+		Long: `
 dsmctl is a utility for interacting with your Synology NAS from a remote terminal
 
-`,
+`}
+	cmd.AddCommand(docker.Cmd(func() api.Client {
+		apiClient, err := api.NewClient(c.Host, c.Account, c.Password)
+		if err != nil {
+			panic(err)
+		}
+		_, err = apiClient.Login()
+		if err != nil {
+			panic(err)
+		}
+		return apiClient
+	}))
+	return cmd
+}
+
+type config struct {
+	Host     string `yaml:"host"`
+	Account  string `yaml:"account"`
+	Password string `yaml:"password"`
 }
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	configFilePath, err := xdg.SearchConfigFile(".dsmctl.yaml")
+	if err != nil {
+		log.Println(`
+couldn't find a .dsmctl.yaml file. You'll need to create one with the following format:
+host: "http://$MY_SYNOLOGY_HOST:5000"
+account: "my-user"
+password: "password"
+`)
+		log.Fatal(err)
+	}
+	open, err := os.Open(configFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var c *config
+	yaml.NewDecoder(open).Decode(&c)
+	if err = rootCmd(c).Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
